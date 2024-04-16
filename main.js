4ace67066119ea370b0b3de38b75ee1dbb4c1c1f
baxtiyorov2013 <baxtiyorov2013>
2024-04-16 10:23:36 +0500

    Succsess

---
+++ main.js
@@ -0,0 +1,10 @@
+let isim = prompt("Ismingizni kriting")
+let id = prompt("Space idingizini kriting")
+let pas = prompt("Space passwordingzini kriting")
+
+let person = {
+    name: isim,
+    spaceId: id, 
+    pasword: pas
+}
+console.log(person);
